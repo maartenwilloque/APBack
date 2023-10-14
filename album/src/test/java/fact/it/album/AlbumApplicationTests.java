f63a17c83a27f86ad9711fbe817292281455ac19
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-10-14 15:16:58 +0200

    init

---
+++ album/src/test/java/fact/it/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ band-service/src/main/java/fact/it/bandservice/BandServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.bandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandServiceApplication.class, args);
+	}
+
+}
