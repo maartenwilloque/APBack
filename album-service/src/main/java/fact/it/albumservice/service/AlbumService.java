6859d9ebaf0fa6b82d8f9d5d444dd9f5bfde38c3
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-10-21 12:29:14 +0200

    album content added

--- album-service/src/main/java/fact/it/albumservice/service/AlbumService.java
+++ album-service/src/main/java/fact/it/albumservice/service/AlbumService.java
@@ -25,21 +25,21 @@ public class AlbumService {
     @PostConstruct
     public void init(){
         Album album = new Album();
-        List<SongDto> songDtoList = new ArrayList<>();
         List<Song> songs = new ArrayList<>();
-        songDtoList.add(new SongDto("Smells Like Teen Spirit", 299, "spotify_id_1"));
-        songDtoList.add(new SongDto("In Bloom", 251, "spotify_id_2"));
-        songDtoList.add(new SongDto("Come as You Are", 219, "spotify_id_3"));
-        songDtoList.add(new SongDto("Breed", 183, "spotify_id_4"));
-        songDtoList.add(new SongDto("Lithium", 262, "spotify_id_5"));
-        songDtoList.add(new SongDto("Polly", 168, "spotify_id_6"));
-        songDtoList.add(new SongDto("Territorial Pissings", 142, "spotify_id_7"));
-        songDtoList.add(new SongDto("Drain You", 224, "spotify_id_8"));
-        songDtoList.add(new SongDto("Lounge Act", 156, "spotify_id_9"));
-        songDtoList.add(new SongDto("Stay Away", 186, "spotify_id_10"));
-        songDtoList.add(new SongDto("On a Plain", 193, "spotify_id_11"));
-        songDtoList.add(new SongDto("Something in the Way", 224, "spotify_id_12"));
-        songDtoList.forEach(songDto ->  songs.add(saveSong(songDto)));
+
+            // Load songs into the list
+            songs.add(new Song(1L, "Smells Like Teen Spirit", 299, "spotify-link-1"));
+            songs.add(new Song(2L, "In Bloom", 251, "spotify-link-2"));
+            songs.add(new Song(3L, "Come as You Are", 219, "spotify-link-3"));
+            songs.add(new Song(4L, "Breed", 183, "spotify-link-4"));
+            songs.add(new Song(5L, "Lithium", 262, "spotify-link-5"));
+            songs.add(new Song(6L, "Polly", 168, "spotify-link-6"));
+            songs.add(new Song(7L, "Territorial Pissings", 142, "spotify-link-7"));
+            songs.add(new Song(8L, "Drain You", 224, "spotify-link-8"));
+            songs.add(new Song(9L, "Lounge Act", 156, "spotify-link-9"));
+            songs.add(new Song(10L, "Stay Away", 186, "spotify-link-10"));
+            songs.add(new Song(11L, "On a Plain", 193, "spotify-link-11"));
+            songs.add(new Song(12L, "Something in the Way", 224, "spotify-link-12"));
         album.setAlbumId("1");
         album.setYear(1991);
         album.setTitle("Nevermind");
@@ -90,13 +90,12 @@ public class AlbumService {
         )).collect(Collectors.toList());
     }
 
-    private Song saveSong(SongDto songDto){
+    private void saveSong(SongDto songDto){
         Song song = new Song();
         song.setDuration(songDto.getDuration());
         song.setTitle(songDto.getTitle());
         song.setSpotifyId(songDto.getSpotifyId());
         songRepository.save(song);
-        return song;
     }
 
 
