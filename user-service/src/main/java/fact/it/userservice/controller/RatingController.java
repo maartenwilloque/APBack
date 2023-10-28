8a3e42a6cdf18ff160144682d3573036bb24b5a9
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-10-28 12:24:43 +0200

    CORS added

--- user-service/src/main/java/fact/it/userservice/controller/RatingController.java
+++ user-service/src/main/java/fact/it/userservice/controller/RatingController.java
@@ -5,7 +5,7 @@ import fact.it.userservice.service.RatingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/rating")
 @RequiredArgsConstructor
